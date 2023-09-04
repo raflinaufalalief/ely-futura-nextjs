d6b4c45a135486ad88e64dc3f740e24da7d19a8a
raflinaufalalief <raflinaufalalief21@gmail.com>
2023-09-04 11:18:22 +0700

    first-commit

--- src/components/Seo.js
+++ src/components/Seo.js
@@ -1,8 +1,10 @@
 // seo.js
 export const defaultSEO = {
-  title: "Punyaasset | home",
-  description: "punyaasset",
-  author: "elyfutura",
-  keywords: "jual beli property",
-  url: "https://www.punyaasset.com",
+  title: "Jual Beli Properti di Punyaasset",
+  description:
+    "Temukan berbagai properti untuk dijual dan beli di Punyaasset. Kami menawarkan beragam pilihan rumah, apartemen, dan tanah di lokasi-lokasi yang strategis.",
+  author: "Elyfutura",
+  keywords:
+    "jual beli properti, properti dijual, apartemen, tanah, investasi properti",
+  url: "https://koleksiproperti.com",
 }
--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -2,21 +2,13 @@ import Head from "next/head"
 import Footer from "@/components/Footer"
 import Navbar from "@/components/Navbar"
 import "@/styles/globals.css"
+import { defaultSEO } from "@/components/Seo"
 
 export default function App({ Component, pageProps }) {
-  // Define default SEO metadata
-  const defaultSEO = {
-    title: "Punyaasset | home",
-    description:
-      "mencari properti yang sesuai dengan keinginan dan kebutuhan Anda",
-    author: "punyaasset",
-    keywords: "jual beli property",
-    url: "https://www.punyaasset.com",
+  const seo = {
+    ...defaultSEO,
+    title: "Punyaasset", // Judul khusus untuk halaman beranda
   }
-
-  // Merge default SEO with page-specific SEO (if available)
-  const seo = { ...defaultSEO, ...pageProps.seo }
-
   return (
     <>
       <Head>
